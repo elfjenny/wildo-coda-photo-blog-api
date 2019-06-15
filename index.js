a61d4e986a0ee692e7338939c4f177370becaa40
Jenny <jennyelfort3@gmail.com>
2019-06-15 14:49:55 +0200

    add request for /api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-app.listen(3000)
+const express = require('express)
+})
 
-
-
+app.get("/api", (req, res)=> {
+    res.send('It works')
+});
 app.listen(3000, function() {
     console.log("Server listing on local host port 3000");
 });
